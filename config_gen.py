f40647a8d0c95d30f55f3cc489ed33bec0b51bcd
daniel280187 <danielmacuare.uk@gmail.com>
2019-03-18 08:48:20 +0000

    Adding function to render the config from a Yaml + Jinja template

--- config_gen.py
+++ config_gen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.6
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 from pprint import pprint
 import yaml
 
@@ -8,9 +8,9 @@ def sect_header(title):
     """
     Prints a section header with the title being passed
     """
-    print("#" * 40)
+    print("#" * 70)
     print(title)
-    print("#" * 40)
+    print("#" * 70)
 
 
 def yaml_to_python(yaml_file):
@@ -22,11 +22,40 @@ def yaml_to_python(yaml_file):
         python_data_strc = yaml.load(yaml_str, Loader=yaml.FullLoader)
     return (yaml_str, python_data_strc)
 
+def yaml_jinja_conf(yaml_file, jinja_templ, cfg_file='output.cfg'):
+    """
+    Combine a Yaml file with a Jinja template and renders a config file.
+    The template must be in the same directory than the script.
+    """
 
-yaml_data, python_data = yaml_to_python("OOB-SW.yaml")
-sect_header("ORIGINAL YAML")
-print(yaml_data)
+    # Reading YAML and trasnforming it into a Python object
+    with open(yaml_file) as f:
+        input_values = yaml.load(f)
+
+    # Defining Jinja Env + jinja template
+    ENV = Environment(loader=FileSystemLoader('.'), trim_blocks=True)
+    template = ENV.get_template(jinja_templ)
+
+    # Rendering the template
+    config = template.render(input_values)
+
+    # Writing config to a file
+    with open(cfg_file, 'w') as output:
+        output.write(config)
+    
+    print(config)    
 
 
-sect_header("CONVERTED TO --> PYTHON DATA STRUCTURE")
-pprint(python_data)
+# PROGRAM
+yaml_jinja_conf('OOB-SW.yaml', 'Initial-config.j2')
+#yaml_data, python_data = yaml_to_python("OOB-SW.yaml")
+
+
+
+#LOGGING
+#sect_header("ORIGINAL YAML")
+#print(yaml_data)
+#sect_header("CONVERTED TO --> PYTHON DATA STRUCTURE")
+#pprint(python_data)
+
+
